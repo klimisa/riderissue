8543104b54fec023e6470a224d11d6936a90138e
Antonios Klimis <antonios.klimis@gmail.com>
2019-10-16 16:37:03 +0300

    first commit

---
+++ RiderIssue.Core/Foo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RiderIssue.Core
+{
+    public enum EnmFoo
+    {
+        A,
+        B
+    }
+    
+    public class Foo
+    {
+        public readonly EnmFoo EnmFooProp;
+
+        public Func<int, string> TheIssue()
+        {
+            return EnmFooProp switch
+            {
+                EnmFoo.A => (_) => "A",
+                EnmFoo.B => (_) => "B",
+                _ => throw new InvalidOperationException()
+            };
+        }
+    }
+}
